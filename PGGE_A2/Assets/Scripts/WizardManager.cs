4ac91ab96ee5f44048a48ea3dc0ba84b1eeb7db1
TeeKaiqi <105273769+TeeKaiqi@users.noreply.github.com>
2024-01-24 00:56:28 +0800

    comments added to wizard manager

--- PGGE_A2/Assets/Scripts/WizardManager.cs
+++ PGGE_A2/Assets/Scripts/WizardManager.cs
@@ -6,23 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class WizardManager : MonoBehaviourPunCallbacks
 {
-    public string mPlayerPrefabName;
-    public PlayerSpawnPoints mSpawnPoints;
+    public string mPlayerPrefabName; //public variable to store name of the player prefab
+    public PlayerSpawnPoints mSpawnPoints; //public variable that stores the info about the spawnpoints
+
     [HideInInspector]
-    public GameObject mPlayerGameObject;
+    public GameObject mPlayerGameObject; //game object that stores the instantiated player charcater
     [HideInInspector]
     private ThirdPersonCamera mThirdPersonCamera;
 
     // Start is called before the first frame update
     private void Start()
     {
-        Transform randomSpawnTransform = mSpawnPoints.GetSpawnPoint();
-        mPlayerGameObject = PhotonNetwork.Instantiate(mPlayerPrefabName,randomSpawnTransform.position,randomSpawnTransform.rotation, 0);
+        Transform randomSpawnTransform = mSpawnPoints.GetSpawnPoint(); //get a random spaw point for the player  by calling the getspawnpoint function from the pawnpoint script
+        mPlayerGameObject = PhotonNetwork.Instantiate(mPlayerPrefabName,randomSpawnTransform.position,randomSpawnTransform.rotation, 0); //instantiate the player prefab
 
-        mThirdPersonCamera = Camera.main.gameObject.AddComponent<ThirdPersonCamera>();
+        mThirdPersonCamera = Camera.main.gameObject.AddComponent<ThirdPersonCamera>(); //add thirdpersoncamera script to main camera
 
-        mPlayerGameObject.GetComponent<WizardMovement>().mFollowCameraForward = false;
-        mThirdPersonCamera.mPlayer = mPlayerGameObject.transform;
+        mPlayerGameObject.GetComponent<WizardMovement>().mFollowCameraForward = false; //makes it so that the players movement doesnt follow the cameras forward direction by setting it false
+        mThirdPersonCamera.mPlayer = mPlayerGameObject.transform; 
         mThirdPersonCamera.mDamping = 20.0f;
         mThirdPersonCamera.mCameraType = CameraType.Follow_Track_Pos_Rot;
     }
@@ -31,13 +32,13 @@ public class WizardManager : MonoBehaviourPunCallbacks
     public void LeaveRoom()
     {
         Debug.LogFormat("LeaveRoom");
-        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LeaveRoom(); //call photon network function to leave the room
     }
 
     public override void OnLeftRoom()
     {
-        Debug.LogFormat("OnLeftRoom()");
-        SceneManager.LoadScene("Menu");
+        Debug.LogFormat("OnLeftRoom()"); 
+        SceneManager.LoadScene("Menu"); //load the menu scene once the player has left the room
     }
 
 }
