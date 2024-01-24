75b29e76789a38353d4a2620f7bb4ffcd10c4d2e
TeeKaiqi <105273769+TeeKaiqi@users.noreply.github.com>
2024-01-24 16:55:15 +0800

    second refactoring and comments

--- PGGE_A2/Assets/Scripts/PlayerState.cs
+++ PGGE_A2/Assets/Scripts/PlayerState.cs
@@ -3,45 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using PGGE.Patterns;
 
-public enum PlayerStateType
+public enum PlayerStateType //enumeration that defines the different player states = moving, attacking and reloading
 {
     MOVEMENT = 0,
     ATTACK,
     RELOAD,
 }
 
-public class PlayerState : FSMState
+public class PlayerState : FSMState //base class for all the player states that inherits from the FSMState
 {
-    protected Player mPlayer = null;
+    protected Player mPlayer = null; //reference to the player
 
-    public PlayerState(Player player) 
-        : base()
+    public PlayerState(Player player) : base() //initialise the player state with a reference to the player
     {
         mPlayer = player;
         mFsm = mPlayer.mFsm;
     }
 
-    public override void Enter()
+    public override void Enter() //This function is called when entering this state
     {
         base.Enter();
     }
-    public override void Exit()
+    public override void Exit() //this function is called when exiting this state
     {
         base.Exit();
     }
-    public override void Update()
+    public override void Update() //the update function will be called every frame when in this particular state
     {
         base.Update();
     }
-    public override void FixedUpdate()
+    public override void FixedUpdate() //this function will be called every fixed frame while while in this state
     {
         base.FixedUpdate();
     }
 }
 
-public class PlayerState_MOVEMENT : PlayerState
+public class PlayerState_MOVEMENT : PlayerState //this is the state that represents the player movement
 {
-    public PlayerState_MOVEMENT(Player player) : base(player)
+    public PlayerState_MOVEMENT(Player player) : base(player) //instantiates the movement state with a reference to the player
     {
         mId = (int)(PlayerStateType.MOVEMENT);
     }
@@ -51,45 +50,28 @@ public class PlayerState_MOVEMENT : PlayerState
         base.Enter();
     }
 
-    public override void Update()
+    public override void Update() //this is the refactored update function, the original code that checks the whether the player is attacking has been moved
     {
         base.Update();
-
-        // For Student ---------------------------------------------------//
-        // Implement the logic of player movement. 
-        //----------------------------------------------------------------//
-        // Hint:
-        //----------------------------------------------------------------//
-        // You should remember that the logic for movement
-        // has already been implemented in PlayerMovement.cs.
-        // So, how do we make use of that?
-        // We certainly do not want to copy and paste the movement 
-        // code from PlayerMovement to here.
-        // Think of a way to call the Move method. 
-        //
-        // You should also
-        // check if fire buttons are pressed so that 
-        // you can transit to ATTACK state.
-
         mPlayer.Move();
+        CheckAndChangeState();
+    }
 
+    private void CheckAndChangeState() //logic that checks for player attacking was put into its own function that is called in the update function
+    {
         for (int i = 0; i < mPlayer.mAttackButtons.Length; ++i)
         {
-            if (mPlayer.mAttackButtons[i])
+            if (mPlayer.mAttackButtons[i]) //if the player is attacking
             {
-                if (mPlayer.mBulletsInMagazine > 0)
+                if (mPlayer.mBulletsInMagazine > 0) //and if the player's bullets is more than 0, then it will become the attack state
                 {
-                    PlayerState_ATTACK attack =
-                  (PlayerState_ATTACK)mFsm.GetState(
-                            (int)PlayerStateType.ATTACK);
-
+                    PlayerState_ATTACK attack = (PlayerState_ATTACK)mFsm.GetState((int)PlayerStateType.ATTACK);
                     attack.AttackID = i;
-                    mPlayer.mFsm.SetCurrentState(
-                        (int)PlayerStateType.ATTACK);
+                    mPlayer.mFsm.SetCurrentState((int)PlayerStateType.ATTACK); //set the state to attack state
                 }
                 else
                 {
-                    Debug.Log("No more ammo left");
+                    Debug.Log("No more ammo left"); //debug that there is no more ammo left
                 }
             }
         }
@@ -101,7 +83,7 @@ public class PlayerState_MOVEMENT : PlayerState
     }
 }
 
-public class PlayerState_ATTACK : PlayerState
+public class PlayerState_ATTACK : PlayerState //state that represents the player attacking
 {
     private int mAttackID = 0;
     private string mAttackName;
@@ -119,7 +101,7 @@ public class PlayerState_ATTACK : PlayerState
         }
     }
 
-    public PlayerState_ATTACK(Player player) : base(player)
+    public PlayerState_ATTACK(Player player) : base(player) //initialise the attack state with a reference to the player
     {
         mId = (int)(PlayerStateType.ATTACK);
     }
@@ -132,90 +114,60 @@ public class PlayerState_ATTACK : PlayerState
     {
         mPlayer.mAnimator.SetBool(mAttackName, false);
     }
-    public override void Update()
+    public override void Update() //update function that was refactored by moving the logic to check ammo and attack into its own function
     {
         base.Update();
+        CheckAmmoAndAttack();
+    }
 
-        // For Student ---------------------------------------------------//
-        // Implement the logic of attack, reload and revert to movement. 
-        //----------------------------------------------------------------//
-        // Hint:
-        //----------------------------------------------------------------//
-        // 1. Transition to RELOAD
-        // Notice that we have three variables, viz., 
-        // mAmunitionCount
-        // mBulletsInMagazine
-        // mMaxAmunitionBeforeReload
-        // You will need to make use of these variables while
-        // implementing the transition to RELOAD.
-        //
-        // 2. Staying in ATTACK state
-        // You should stay in ATTACK state as long as the 
-        // Fire buttons are pressed. During ATTACK state
-        // you should trigger the correct ATTACK animation
-        // based on which button is pressed and shoot bullets.
-        // Every bullet shot should reduce the count of mAmunitionCount
-        // and mBulletsInMagazine.
-        // Once mBulletsInMagazine reaches to 0 you should 
-        // transit to RELOAD state.
-        //
-        // 3. Transition to MOVEMENT state
-        // You should transit to MOVEMENT state when any of the 
-        // following two situations happen.
-        // First you have exhausted all your bullets, that means your
-        // mAmunitionCount is 0 or if you do not press any of the
-        // Fire buttons.
-        // Discuss with your tutor if you find any difficulties
-        // in implementing this section.        
-        
-        // For tutor - start ---------------------------------------------//
+    private void CheckAmmoAndAttack()
+    {
         Debug.Log("Ammo count: " + mPlayer.mAmunitionCount + ", In Magazine: " + mPlayer.mBulletsInMagazine);
         if (mPlayer.mBulletsInMagazine == 0 && mPlayer.mAmunitionCount > 0)
         {
-            mPlayer.mFsm.SetCurrentState((int)PlayerStateType.RELOAD);
+            mPlayer.mFsm.SetCurrentState((int)PlayerStateType.RELOAD); //transitions to the reload state if the payer is out of bullets but there is still ammunition
             return;
         }
 
         if (mPlayer.mAmunitionCount <= 0 && mPlayer.mBulletsInMagazine <= 0)
         {
-            mPlayer.mFsm.SetCurrentState((int)PlayerStateType.MOVEMENT);
-            mPlayer.NoAmmo();
+            mPlayer.mFsm.SetCurrentState((int)PlayerStateType.MOVEMENT); //transitions to the movement state when there is no bullets and no ammunition
+            mPlayer.NoAmmo(); //calls the noammo function 
             return;
         }
 
-        if (mPlayer.mAttackButtons[mAttackID])
+        if (mPlayer.mAttackButtons[mAttackID]) //if the attack button was pressed, perform the attack
         {
             mPlayer.mAnimator.SetBool(mAttackName, true);
             mPlayer.Fire(AttackID);
         }
         else
         {
-            mPlayer.mAnimator.SetBool(mAttackName, false);
+            mPlayer.mAnimator.SetBool(mAttackName, false); //else set the attack animation to false and transition to movement state
             mPlayer.mFsm.SetCurrentState((int)PlayerStateType.MOVEMENT);
         }
-        // For tutor - end   ---------------------------------------------//
     }
 }
 
-public class PlayerState_RELOAD : PlayerState
+public class PlayerState_RELOAD : PlayerState //state that represents the player reloading
 {
     public float ReloadTime = 3.0f;
     float dt = 0.0f;
     public int previousState;
-    public PlayerState_RELOAD(Player player) : base(player)
+    public PlayerState_RELOAD(Player player) : base(player) //initialises the reload state with a reference to the player
     {
         mId = (int)(PlayerStateType.RELOAD);
     }
 
     public override void Enter()
     {
-        mPlayer.mAnimator.SetTrigger("Reload");
+        mPlayer.mAnimator.SetTrigger("Reload"); //set trigger for the reload animation 
         mPlayer.Reload();
         dt = 0.0f;
     }
     public override void Exit()
     {
-        if (mPlayer.mAmunitionCount > mPlayer.mMaxAmunitionBeforeReload)
+        if (mPlayer.mAmunitionCount > mPlayer.mMaxAmunitionBeforeReload) //login that calculates the bullets in the magazine and ammunition ased on the reload
         {
             mPlayer.mBulletsInMagazine += mPlayer.mMaxAmunitionBeforeReload;
             mPlayer.mAmunitionCount -= mPlayer.mBulletsInMagazine;
